becae32667a13a351930fadcb9f143b34be0aaf3
Neal Stewart <neal@goinstant.com>
2012-03-02 21:16:59 -0400

    Bunch of junk unhelpful commit msg

--- network.js
+++ network.js
@@ -1,14 +1,11 @@
-var app = require('express').createServer();
+var app = require('./lib/server');
 var io = require('socket.io').listen(app);
 
 io.configure(function() {
   io.set('transports', ['websocket']);
 });
 
-var setupApp = require('./lib/server').setup;
 var setupSockets = require('./lib/sockets').setup;
-
-setupApp(app);
 setupSockets(io);
 
 app.listen(80);
